20fe47570b7ac01b15ef62999ad5c8957dc4d6e2
ramon oliveira <ramon30012000@gmail.com>
2022-04-29 11:05:57 -0300

    test: add sign-in e2e test

---
+++ cypress/integration/signUp.spec.js
@@ -0,0 +1,26 @@
+import "./cypress.js";
+
+import { faker } from "@faker-js/faker";
+describe("Sign-up", () => {
+
+  it("should sign-up successfully", () => {
+    const user = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    cy.visit("http://localhost:3000/");
+
+    cy.get("input[name=email]").type(user.email);
+    cy.get("input[name=password]").type(user.password);
+    cy.get("input[name=passwordConfirmation]").type(user.password);
+    
+    cy.intercept("POST", "http://localhost:5000/sign-up").as("signUp");
+    
+    cy.contains("Cadastrar").click();
+
+    cy.wait("@signUp");
+
+    cy.contains("Cadastro efetuado com sucesso!");
+  });
+});
